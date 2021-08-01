84727b13f3f2fb3f874428941e1de1d22625cb02
Zane Gambrill <zanegam@gmail.com>
2021-08-01 19:51:24 -0400

    Random color working

--- scripts.js
+++ scripts.js
@@ -105,5 +105,14 @@ function paintWhite(e){
 }
 
 function paintRandom(e){
-    e.target.style.backgroundColor = "orange";
+    e.target.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor(){
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
